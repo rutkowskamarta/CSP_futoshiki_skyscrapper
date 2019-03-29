62197f70cb28aad7a0e1099744e42cb5a38856b5
Marta Rutkowska <marta.rutkowska@protonmail.com>
2019-03-29 15:13:49 +0100

    initial commit

---
+++ CSP_futoshiki_skyscrapper/CSP_futoshiki_skyscrapper/DataLoader.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSP_futoshiki_skyscrapper
+{
+    class DataLoader
+    {
+        //private delegate
+    }
+}
---
+++ CSP_futoshiki_skyscrapper/CSP_futoshiki_skyscrapper/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSP_futoshiki_skyscrapper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
